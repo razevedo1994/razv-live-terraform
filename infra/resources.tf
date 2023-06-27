7a8afbe5e9e0d4b5c2be72a1b30479d875575fef
razevedo1994 <razevedo.contato@gmail.com>
2023-06-27 00:40:55 -0300

    Define policy

--- infra/resources.tf
+++ infra/resources.tf
@@ -1,3 +1,4 @@
+# Lambda section
 data "aws_iam_policy_document" "assume_role" {
     statement {
       effect = "Allow"
@@ -11,9 +12,28 @@ data "aws_iam_policy_document" "assume_role" {
     }
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-    name                = "iam_for_lambda"
+resource "aws_iam_role" "role" {
+    name                = "role_live_terraform"
     assume_role_policy  = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "policy_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3-object-lambda:PutObject"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "policy_live_terraform"
+  description = "Policy for lambda function"
+  policy      = data.aws_iam_policy_document.policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
 }
 
 data "archive_file" "lambda" {
@@ -25,10 +45,16 @@ data "archive_file" "lambda" {
 resource "aws_lambda_function" "live_lambda_example" {
   filename      = "./lambda_layer/lambda_function_payload.zip"
   function_name = "ingest_data_from_api"
-  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "handler.soma"
+  role          = aws_iam_role.role.arn
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "python3.9"
+
+  depends_on = [ 
+    aws_iam_role_policy_attachment.attach
+   ]
 }
+
+# 
