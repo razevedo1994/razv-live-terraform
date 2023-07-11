225e03eaf6bc78507c0202e8ae5652ed704ca713
razevedo1994 <razevedo.contato@gmail.com>
2023-07-10 21:10:09 -0300

    Update vpc configurations

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "this" {
 
 resource "aws_subnet" "this" {
   vpc_id                    = aws_vpc.this.id
-  cidr_block                = "10.0.0.0/8"
+  cidr_block                = "10.0.0.0/24"
   availability_zone         = "us-east-1a"
   map_public_ip_on_launch   = false
 
