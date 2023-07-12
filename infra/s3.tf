964940fdb36ebf450bc141f16446d15a1c4f9d01
razevedo1994 <razevedo.contato@gmail.com>
2023-07-12 09:45:33 -0300

    Include acl rule

--- infra/s3.tf
+++ infra/s3.tf
@@ -6,6 +6,20 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.this]
+
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
     bucket = aws_s3_bucket.this.id
 
@@ -16,10 +30,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
     }
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access" {
-    bucket                  = aws_s3_bucket.this.id
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-}
+# resource "aws_s3_bucket_public_access_block" "public_access" {
+#     bucket                  = aws_s3_bucket.this.id
+#     block_public_acls       = true
+#     block_public_policy     = true
+#     ignore_public_acls      = true
+#     restrict_public_buckets = true
+# }
