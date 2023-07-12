3724350ba9761ef24760be94b3d23d7c9a141b7d
razevedo1994 <razevedo.contato@gmail.com>
2023-07-12 20:06:12 -0300

    Remove comments

--- infra/s3.tf
+++ infra/s3.tf
@@ -29,11 +29,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
       }
     }
 }
-
-# resource "aws_s3_bucket_public_access_block" "public_access" {
-#     bucket                  = aws_s3_bucket.this.id
-#     block_public_acls       = true
-#     block_public_policy     = true
-#     ignore_public_acls      = true
-#     restrict_public_buckets = true
-# }
